df8f024d1f859da3af57c70008cf48883ecf35bf
bdarcus <bdarcus@gmail.com>
2009-05-13 09:28:43 -0400

    fixed sets deprecation issue

--- todo.py
+++ todo.py
@@ -105,7 +105,7 @@ WIN_GREY     = 0x08
 ############################################################
 # Be careful changing things below here
 
-import re, os, sys, time, getopt, sets, subprocess
+import re, os, sys, time, getopt, subprocess
 from shutil import copyfile
 
 # Guess the pager!
@@ -804,7 +804,7 @@ def edit(file):
 def removeDuplicates():
     """Removes duplicate lines in the TODO file"""
     taskCopy = getTaskDict()
-    theSet = sets.Set(taskCopy.values())
+    theSet = set(taskCopy.values())
     dupCount = len(taskCopy) - len(theSet)
     if dupCount > 0:
         count = 0
